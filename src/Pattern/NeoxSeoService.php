6f8657c910317ceca086ccbe94ef983bf8d3f75d
Xorg <xorg@neonatis.com>
2023-09-30 09:56:38 +0200

    Optimize factory ...

--- src/Pattern/NeoxSeoService.php
+++ src/Pattern/NeoxSeoService.php
@@ -1,57 +1,40 @@
 <?php
     
     namespace NeoxSeo\NeoxSeoBundle\Pattern;
-
+    
     use NeoxSeo\NeoxSeoBundle\Attribute\NeoxSeo;
     use NeoxSeo\NeoxSeoBundle\Model\SeoBag;
     use ReflectionClass;
-    use ReflectionException;
     use ReflectionMethod;
     use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
     use Symfony\Component\HttpFoundation\RequestStack;
     
     class NeoxSeoService
     {
-        private RequestStack $requestStack;
+        public ?SeoBag $seoBag = null;
         private string $controller;
         private string $action;
-        public ?SeoBag $seoBag = null;
-        private ParameterBagInterface $parameterBag;
+     
         
-        public function __construct(RequestStack $requestStack, ParameterBagInterface $parameterBag )
+        public function __construct(private readonly RequestStack $requestStack, private readonly ParameterBagInterface $parameterBag)
         {
-            $this->requestStack = $requestStack;
-            $this->parameterBag = $parameterBag;
+       
         }
         
-        /**
-         * @throws ReflectionException
-         */
         public function setNeoxSeo(): SeoBag
         {
-            $this->getInfoAboutCurrentRequest();
             
-            $classAttributes    = (new ReflectionClass($this->controller))->getAttributes(NeoxSeo::class);
-            foreach ($classAttributes as $class) {
-                foreach ($class->newInstance() as $key => $value) {
+            
+            $attributes = $this->getAttributesFromControllerAndMethod();
+            
+            foreach ($attributes as $attribute) {
+                foreach ($attribute->newInstance() as $key => $value) {
                     if ($value) {
                         $setter = "set" . $key;
                         $this->seoBag->$setter($value);
                     }
                 }
             }
-            
-            $methodAttributes    = (new ReflectionMethod($this->controller, $this->action))->getAttributes(NeoxSeo::class);
-            foreach ($methodAttributes as $method) {
-                foreach ($method->newInstance() as $key => $value) {
-                    if ($value) {
-                        $setter = "set" . $key;
-                        $this->seoBag->$setter($value);
-                    }
-                }
-            }
-            
-            
             return $this->seoBag;
         }
         
@@ -60,9 +43,6 @@
             return $this->seoBag;
         }
         
-        /**
-         * @throws ReflectionException
-         */
         public function init(): seoBag
         {
             // Get the SEO configuration
@@ -85,6 +65,14 @@
             return $this->seoBag;
         }
         
+        private function getAttributesFromControllerAndMethod(): array
+        {
+            $this->getInfoAboutCurrentRequest();
+            $classAttributes = (new ReflectionClass($this->controller))->getAttributes(NeoxSeo::class);
+            $methodAttributes = (new ReflectionMethod($this->controller, $this->action))->getAttributes(NeoxSeo::class);
+            
+            return array_merge($classAttributes, $methodAttributes);
+        }
         
         private function getInfoAboutCurrentRequest(): void
         {
@@ -92,8 +80,7 @@
             
             if ($request) {
                 $controllerName = $request->attributes->get('_controller');
-                list($this->controller, $this->action ) = explode('::', $controllerName);
+                list($this->controller, $this->action) = explode('::', $controllerName);
             }
-            
         }
     }
